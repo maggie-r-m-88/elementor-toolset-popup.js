fbe1f7731f819e4a294c252679101d35b07092c1
Maggie Martin <8229633+maggie-r-m-88@users.noreply.github.com>
2020-05-12 12:25:20 +0200

    clean up unused functions

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-/* this works!! */
 jQuery(document).ready(function($){
 
       function getProjects() {
                           
             var chosen = this.value
             if (chosen == "vendita"){
-                console.log(chosen + " is selected")
                 jQuery(".elementor-location-popup .top-search #filter_rp").show();
                 jQuery(".elementor-location-popup .top-search #filter_sp").hide();
                 
             }
             if (chosen == "affitto"){
-                console.log(chosen + " is selected")
                 jQuery(".elementor-location-popup .top-search #filter_rp").hide();
                 jQuery(".elementor-location-popup .top-search #filter_sp").show();
                                 
@@ -21,13 +18,11 @@ jQuery(document).ready(function($){
       function staticVersion(){
             var chosen = this.value
             if (chosen == "vendita"){
-                console.log(chosen + " is selected")
                 jQuery(".elementor-widget-toggle .top-search #filter_rp").show();
                 jQuery(".elementor-widget-toggle .top-search #filter_sp").hide();
                 
             }
             if (chosen == "affitto"){
-                console.log(chosen + " is selected")
                 jQuery(".elementor-widget-toggle .top-search #filter_rp").hide();
                 jQuery(".elementor-widget-toggle .top-search #filter_sp").show();
                                 
@@ -37,16 +32,10 @@ jQuery(document).ready(function($){
    
 
       jQuery( document ).on( 'elementor/popup/show', () => {
-            
-          jQuery(".elementor-location-popup").on("change", ".js-wpv-filter-trigger", function() {
-                  // here the value will updated anytime when button was clicked
-                      //getProjects();
-          });
-
+ 
           jQuery(document).on("change", ".js-wpv-filter-trigger", getProjects);
           
-                
-       
-   });
+               
+      });
 
 });
